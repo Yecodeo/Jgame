a8d3cd9f9152336ecb472fd98986bcfd0f2eb540
yecodeo <yecodeo@gmail.com>
2020-07-31 04:10:51 +0200

    crlr to lf

--- src/utils/template.js
+++ src/utils/template.js
@@ -1,22 +1,29 @@
+/**
+ * create card node to add
+ * @param {*} props *
+ * @return {NodeDOM}
+ */
 export default function card(props) {
 	const flip = factory('div', ['m-1', 'flip']);
 	const c = factory('div', ['carte']);
-	const hidden =  factory('div', ['hiddens']); 
-	const picture = factory('div', ['picture', props]); 
+	const hidden = factory('div', ['hiddens']);
+	const picture = factory('div', ['picture', props]);
 
 	hidden.append(picture);
 	c.append(hidden);
 	flip.append(c);
 	return flip;
 }
+
 /**
  * create element and it class name
- * @param {node type} type 
- * @param {class name} className 
+ * @param {*} type
+ * @param {*} className
+ * @return {nodeDOm}
  */
 function factory(type, className = []) {
 	if (type && className) {
-		let el = document.createElement(type);
+		const el = document.createElement(type);
 		for (const n of className) {
 			el.classList.add(n);
 		}
